7914359c71d5e5c9a0092257a565fe53fe2b7684
Adam Graham <adam@zigurous.com>
2021-02-03 18:50:48 -0600

    Add equality comparer for float values

---
+++ Runtime/FloatEqualityComparer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Zigurous.Math
+{
+    public sealed class FloatEqualityComparer : IEqualityComparer<float>
+    {
+        public int digits = 3;
+
+        public bool Equals(float x, float y) => System.Math.Round(x, this.digits) == System.Math.Round(y, this.digits);
+
+        public int GetHashCode(float f) => System.Math.Round(f, this.digits).GetHashCode();
+
+    }
+
+}
