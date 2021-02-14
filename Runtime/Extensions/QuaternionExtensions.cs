6dc6664816c05ecb0483d850d4df6723316b954b
Adam Graham <adam@zigurous.com>
2021-02-13 20:11:19 -0600

    Adjust Quaternion SmoothDamp math

--- Runtime/Extensions/QuaternionExtensions.cs
+++ Runtime/Extensions/QuaternionExtensions.cs
@@ -7,11 +7,11 @@ namespace Zigurous.Math
         public static Quaternion SmoothDamp(this Quaternion current, Quaternion target, ref Quaternion currentVelocity, float smoothTime, float maxSpeed = Mathf.Infinity)
         {
             float dot = Quaternion.Dot(current, target);
-            float multiple = dot > 0.0f ? 1.0f : -1.0f;
-            target.x *= multiple;
-            target.y *= multiple;
-            target.z *= multiple;
-            target.w *= multiple;
+            float direction = dot > 0.0f ? 1.0f : -1.0f;
+            target.x *= direction;
+            target.y *= direction;
+            target.z *= direction;
+            target.w *= direction;
 
             Vector4 result = new Vector4(
                 x: Mathf.SmoothDamp(current.x, target.x, ref currentVelocity.x, smoothTime, maxSpeed),
@@ -19,11 +19,11 @@ namespace Zigurous.Math
                 z: Mathf.SmoothDamp(current.z, target.z, ref currentVelocity.z, smoothTime, maxSpeed),
                 w: Mathf.SmoothDamp(current.w, target.w, ref currentVelocity.w, smoothTime, maxSpeed)).normalized;
 
-            float inverseDeltaTime = 1.0f / Time.deltaTime;
-            currentVelocity.x = (result.x - current.x) * inverseDeltaTime;
-            currentVelocity.y = (result.y - current.y) * inverseDeltaTime;
-            currentVelocity.z = (result.z - current.z) * inverseDeltaTime;
-            currentVelocity.w = (result.w - current.w) * inverseDeltaTime;
+            Vector4 error = Vector4.Project(new Vector4(currentVelocity.x, currentVelocity.y, currentVelocity.z, currentVelocity.w), result);
+            currentVelocity.x -= error.x;
+            currentVelocity.y -= error.y;
+            currentVelocity.z -= error.z;
+            currentVelocity.w -= error.w;
 
             return new Quaternion(result.x, result.y, result.z, result.w);
         }
