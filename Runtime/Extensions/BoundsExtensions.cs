c87a24c710d9c6d454bee000009e66d982ba5ab7
Adam Graham <adam@zigurous.com>
2021-02-27 23:59:28 -0600

    Add Bounds and Collider extensions to generate random points

---
+++ Runtime/Extensions/BoundsExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Zigurous.Math
+{
+    public static class BoundsExtensions
+    {
+        public static Vector3 RandomPointInside(this Bounds bounds)
+        {
+            return new Vector3(
+                Random.Range(bounds.min.x, bounds.max.x),
+                Random.Range(bounds.min.y, bounds.max.y),
+                Random.Range(bounds.min.z, bounds.max.z));
+        }
+
+    }
+
+}
---
+++ Runtime/Extensions/ColliderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Zigurous.Math
+{
+    public static class ColliderExtensions
+    {
+        public static Vector3 RandomPointInside(this Collider collider)
+        {
+            Vector3 randomPoint = collider.bounds.RandomPointInside();
+            Vector3 closetPoint = collider.ClosestPoint(randomPoint);
+
+            if (closetPoint == randomPoint) { // inside
+                return randomPoint;
+            } else { // outside
+                return Vector3.Lerp(closetPoint, collider.bounds.center, Random.value);
+            }
+        }
+
+        public static Vector3 RandomPointInside(this Collider2D collider)
+        {
+            Vector3 randomPoint = collider.bounds.RandomPointInside();
+            Vector3 closetPoint = collider.ClosestPoint(randomPoint);
+
+            if (closetPoint == randomPoint) { // inside
+                return randomPoint;
+            } else { // outside
+                return Vector3.Lerp(closetPoint, collider.bounds.center, Random.value);
+            }
+        }
+
+    }
+
+}
