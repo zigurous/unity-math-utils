7a95a002da711a3483b922580ad78d0c0a34404e
Adam Graham <adam@zigurous.com>
2021-07-12 15:42:27 -0500

    Add BoundsInt.RandomPointInside extension

--- Runtime/Extensions/BoundsExtensions.cs
+++ Runtime/Extensions/BoundsExtensions.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 namespace Zigurous.Math
 {
@@ -7,6 +7,18 @@ namespace Zigurous.Math
         public static Vector3 RandomPointInside(this Bounds bounds)
         {
             return new Vector3(
+                Random.Range(bounds.min.x, bounds.max.x),
+                Random.Range(bounds.min.y, bounds.max.y),
+                Random.Range(bounds.min.z, bounds.max.z));
+        }
+
+        /// <summary>
+        /// Returns a random point inside the bounds.
+        /// </summary>
+        /// <param name="bounds">The bounds to get a random point from.</param>
+        public static Vector3Int RandomPointInside(this BoundsInt bounds)
+        {
+            return new Vector3Int(
                 Random.Range(bounds.min.x, bounds.max.x),
                 Random.Range(bounds.min.y, bounds.max.y),
                 Random.Range(bounds.min.z, bounds.max.z));
