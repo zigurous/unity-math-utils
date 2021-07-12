4fe80bb8cc74ab33689302ba0bbd67ad21b7fa71
Adam Graham <adam@zigurous.com>
2021-07-12 15:40:56 -0500

    Move dice rolls to its own static class

--- Runtime/Chance.cs
+++ Runtime/Chance.cs
@@ -1,17 +1,24 @@
 ﻿namespace Zigurous.Math
 {
     /// <summary>
-    /// Provides functions for generating random chance values.
+    /// Functions for generating random chance values.
     /// </summary>
     public static class Chance
     {
+        /// <summary>
+        /// The sides of a coin.
+        /// </summary>
         public enum Coin { Heads, Tails }
 
-        public enum GamblingColor { Red, Black, White, Green }
+        /// <summary>
+        /// The suits of a standard 52-card deck.
+        /// </summary>
+        public enum Suit { Hearts, Diamonds, Clubs, Spades }
 
-        public enum PlayingCardSuit { Hearts, Diamonds, Spades, Clubs }
-
-        public enum PlayingCard
+        /// <summary>
+        /// The playing cards in a standard 52-card deck.
+        /// </summary>
+        public enum Card
         {
             HeartsA, HeartsK, HeartsQ, HeartsJ, Hearts10, Hearts9, Hearts8,
             Hearts7, Hearts6, Hearts5, Hearts4, Hearts3, Hearts2,
@@ -20,209 +27,58 @@
             SpadesA, SpadesK, SpadesQ, SpadesJ, Spades10, Spades9, Spades8,
             Spades7, Spades6, Spades5, Spades4, Spades3, Spades2,
             ClubsA, ClubsK, ClubsQ, ClubsJ, Clubs10, Clubs9, Clubs8,
-            Clubs7, Clubs6, Clubs5, Clubs4, Clubs3, Clubs2
+            Clubs7, Clubs6, Clubs5, Clubs4, Clubs3, Clubs2,
+            Joker, None,
         }
 
-        public static readonly PlayingCardSuit[] PLAYING_CARD_SUITES = {
-            PlayingCardSuit.Hearts,
-            PlayingCardSuit.Diamonds,
-            PlayingCardSuit.Spades,
-            PlayingCardSuit.Clubs
-        };
-
-        public static readonly PlayingCard[] PLAYING_CARDS = {
-            PlayingCard.HeartsA, PlayingCard.HeartsK, PlayingCard.HeartsQ, PlayingCard.HeartsJ, PlayingCard.Hearts10, PlayingCard.Hearts9, PlayingCard.Hearts8,
-            PlayingCard.Hearts7, PlayingCard.Hearts6, PlayingCard.Hearts5, PlayingCard.Hearts4, PlayingCard.Hearts3, PlayingCard.Hearts2,
-            PlayingCard.DiamondsA, PlayingCard.DiamondsK, PlayingCard.DiamondsQ, PlayingCard.DiamondsJ, PlayingCard.Diamonds10, PlayingCard.Diamonds9, PlayingCard.Diamonds8,
-            PlayingCard.Diamonds7, PlayingCard.Diamonds6, PlayingCard.Diamonds5, PlayingCard.Diamonds4, PlayingCard.Diamonds3, PlayingCard.Diamonds2,
-            PlayingCard.SpadesA, PlayingCard.SpadesK, PlayingCard.SpadesQ, PlayingCard.SpadesJ, PlayingCard.Spades10, PlayingCard.Spades9, PlayingCard.Spades8,
-            PlayingCard.Spades7, PlayingCard.Spades6, PlayingCard.Spades5, PlayingCard.Spades4, PlayingCard.Spades3, PlayingCard.Spades2,
-            PlayingCard.ClubsA, PlayingCard.ClubsK, PlayingCard.ClubsQ, PlayingCard.ClubsJ, PlayingCard.Clubs10, PlayingCard.Clubs9, PlayingCard.Clubs8,
-            PlayingCard.Clubs7, PlayingCard.Clubs6, PlayingCard.Clubs5, PlayingCard.Clubs4, PlayingCard.Clubs3, PlayingCard.Clubs2
-        };
-
         /// <summary>
-        /// Returns a random value from an array with probability proportional
-        /// to a given set of frequencies.
+        /// Returns a random playing card suit.
         /// </summary>
-        public static T Roll<T>(T[] values, int[] frequencies)
+        public static Suit RandomSuit()
         {
-            T[] extendedRange = new T[frequencies.Sum()];
-
-            int length = System.Math.Min(values.Length, frequencies.Length);
-            int count = 0;
-
-            for (int i = 0; i < length; i++)
-            {
-                T value = values[i];
-                int frequency = frequencies[i];
-
-                while (--frequency > 0) {
-                    extendedRange[count++] = value;
-                }
-            }
-
-            return extendedRange[UnityEngine.Random.Range(0, extendedRange.Length)];
+            return (Suit)UnityEngine.Random.Range(0, 4);
         }
 
         /// <summary>
-        /// Returns a random number on a 4-sided dice (1-4).
+        /// Returns a random card from a standard 52-card deck.
         /// </summary>
-        public static int D4() => UnityEngine.Random.Range(1, 5);
-
-        /// <summary>
-        /// Returns the sum of rolling a 4-sided dice (1-4), x amount of times.
-        /// </summary>
-        public static int D4(int x)
+        public static Card RandomCard()
         {
-            int roll = 0;
-            while (x-- > 0) roll += D4();
-            return roll;
+            return (Card)UnityEngine.Random.Range(0, 52);
         }
 
         /// <summary>
-        /// Returns a random number on a 6-sided dice (1-6).
+        /// Returns <c>true</c> or <c>false</c>, with 50-50 odds.
         /// </summary>
-        public static int D6() => UnityEngine.Random.Range(1, 7);
-
-        /// <summary>
-        /// Returns the sum of rolling a 6-sided dice (1-6), x amount of times.
-        /// </summary>
-        public static int D6(int x)
+        public static bool RandomBool()
         {
-            int roll = 0;
-            while (x-- > 0) roll += D6();
-            return roll;
+            return UnityEngine.Random.value < 0.5f;
         }
 
         /// <summary>
-        /// Returns a random number on an 8-sided dice (1-8).
+        /// Returns <c>1.0f</c> or <c>-1.0f</c>, with 50-50 odds.
         /// </summary>
-        public static int D8() => UnityEngine.Random.Range(1, 9);
-
-        /// <summary>
-        /// Returns the sum of rolling an 8-sided dice (1-8), x amount of times.
-        /// </summary>
-        public static int D8(int x)
+        public static float PositiveOrNegative()
         {
-            int roll = 0;
-            while (x-- > 0) roll += D8();
-            return roll;
+            return UnityEngine.Random.value < 0.5f ? 1.0f : -1.0f;
         }
 
         /// <summary>
-        /// Returns a random number on a 10-sided dice (1-10).
+        /// Returns <c>-value</c> or <c>+value</c>, with 50-50 odds.
         /// </summary>
-        public static int D10() => UnityEngine.Random.Range(1, 11);
-
-        /// <summary>
-        /// Returns the sum of rolling a 10-sided dice (1-10), x amount of
-        /// times.
-        /// </summary>
-        public static int D10(int x)
+        /// <param name="value">The value to be returned.</param>
+        public static float PositiveOrNegative(float value)
         {
-            int roll = 0;
-            while (x-- > 0) roll += D10();
-            return roll;
+            return UnityEngine.Random.value < 0.5f ? value : -value;
         }
 
         /// <summary>
-        /// Returns a random number on a 12-sided dice (1-12).
+        /// Returns <c>Heads</c> or <c>Tails</c>, with 50-50 odds.
         /// </summary>
-        public static int D12() => UnityEngine.Random.Range(1, 13);
-
-        /// <summary>
-        /// Returns the sum of rolling a 12-sided dice (1-12), x amount of
-        /// times.
-        /// </summary>
-        public static int D12(int x)
+        public static Coin CoinFlip()
         {
-            int roll = 0;
-            while (x-- > 0) roll += D12();
-            return roll;
+            return UnityEngine.Random.value < 0.5f ? Coin.Heads : Coin.Tails;
         }
-
-        /// <summary>
-        /// Returns a random number on a 20-sided dice (1-20).
-        /// </summary>
-        public static int D20() => UnityEngine.Random.Range(1, 21);
-
-        /// <summary>
-        /// Returns the sum of rolling a 20-sided dice (1-20), x amount of
-        /// times.
-        /// </summary>
-        public static int D20(int x)
-        {
-            int roll = 0;
-            while (x-- > 0) roll += D20();
-            return roll;
-        }
-
-        /// <summary>
-        /// Returns a random number on a n-sided dice (1-n).
-        /// </summary>
-        public static int NSidedDiceRoll(int n) => UnityEngine.Random.Range(1, n);
-
-        /// <summary>
-        /// Returns the sum of rolling a n-sided dice (1-n), x amount of times.
-        /// </summary>
-        public static int NSidedDiceRoll(int n, int x)
-        {
-            int roll = 0;
-            while (x-- > 0) roll += NSidedDiceRoll(n);
-            return roll;
-        }
-
-        /// <summary>
-        /// Returns a random number on a dice with custom values.
-        /// </summary>
-        public static int CustomDiceRoll(int[] values) => values[UnityEngine.Random.Range(0, values.Length)];
-
-        /// <summary>
-        /// Returns the sum of rolling a dice with custom values, x amount of
-        /// times.
-        /// </summary>
-        public static int CustomDiceRoll(int[] values, int x)
-        {
-            int roll = 0;
-            while (x-- > 0) roll += CustomDiceRoll(values);
-            return roll;
-        }
-
-        /// <summary>
-        /// Returns true or false, with 50-50 odds.
-        /// </summary>
-        public static bool RandomBool() => UnityEngine.Random.value < 0.5f;
-
-        /// <summary>
-        /// Returns "heads" or "tails", with 50-50 odds.
-        /// </summary>
-        public static Coin CoinFlip() => UnityEngine.Random.value < 0.5f ? Coin.Heads : Coin.Tails;
-
-        /// <summary>
-        /// Returns 1.0f or -1.0f, with 50-50 odds.
-        /// </summary>
-        public static float PositiveOrNegative() => UnityEngine.Random.value < 0.5f ? 1.0f : -1.0f;
-
-        /// <summary>
-        /// Returns -value or +value, with 50-50 odds.
-        /// </summary>
-        public static float PositiveOrNegative(float value) => UnityEngine.Random.value < 0.5f ? value : -value;
-
-        /// <summary>
-        /// Returns "red" or "black", with 50-50 odds.
-        /// </summary>
-        public static GamblingColor RedBlack() => (UnityEngine.Random.Range(0, 2) == 1) ? GamblingColor.Red : GamblingColor.Black;
-
-        /// <summary>
-        /// Returns a random playing card from a standard deck of 52 cards.
-        /// </summary>
-        public static PlayingCard RandomPlayingCard() => PLAYING_CARDS[UnityEngine.Random.Range(0, 52)];
-
-        /// <summary>
-        /// Returns "hearts", "diamonds", "spades", or "clubs", with equal odds.
-        /// </summary>
-        public static PlayingCardSuit RandomPlayingCardSuite() => PLAYING_CARD_SUITES[UnityEngine.Random.Range(0, 4)];
 
     }
 
---
+++ Runtime/Dice.cs
@@ -0,0 +1,213 @@
+﻿namespace Zigurous.Math
+{
+    /// <summary>
+    /// Functions for rolling dice.
+    /// </summary>
+    public static class Dice
+    {
+        /// <summary>
+        /// Returns a random number on a 4-sided dice [1..4].
+        /// </summary>
+        public static int D4()
+        {
+            return UnityEngine.Random.Range(1, 5);
+        }
+
+        /// <summary>
+        /// Returns the sum of rolling a 4-sided dice [1..4]
+        /// <paramref name="n"/> times.
+        /// </summary>
+        /// <param name="n">The number of times to roll the dice.</param>
+        public static int D4(int n)
+        {
+            int roll = 0;
+            while (n-- > 0) roll += D4();
+            return roll;
+        }
+
+        /// <summary>
+        /// Returns a random number on a 6-sided dice [1..6].
+        /// </summary>
+        public static int D6()
+        {
+            return UnityEngine.Random.Range(1, 7);
+        }
+
+        /// <summary>
+        /// Returns the sum of rolling a 6-sided dice [1..6]
+        /// <paramref name="n"/> times.
+        /// </summary>
+        /// <param name="n">The number of times to roll the dice.</param>
+        public static int D6(int n)
+        {
+            int roll = 0;
+            while (n-- > 0) roll += D6();
+            return roll;
+        }
+
+        /// <summary>
+        /// Returns a random number on an 8-sided dice [1..8].
+        /// </summary>
+        public static int D8()
+        {
+            return UnityEngine.Random.Range(1, 9);
+        }
+
+        /// <summary>
+        /// Returns the sum of rolling an 8-sided dice [1..8]
+        /// <paramref name="n"/> times.
+        /// </summary>
+        /// <param name="n">The number of times to roll the dice.</param>
+        public static int D8(int n)
+        {
+            int roll = 0;
+            while (n-- > 0) roll += D8();
+            return roll;
+        }
+
+        /// <summary>
+        /// Returns a random number on a 10-sided dice [1..10].
+        /// </summary>
+        public static int D10()
+        {
+            return UnityEngine.Random.Range(1, 11);
+        }
+
+        /// <summary>
+        /// Returns the sum of rolling a 10-sided dice [1..10]
+        /// <paramref name="n"/> times.
+        /// </summary>
+        /// <param name="n">The number of times to roll the dice.</param>
+        public static int D10(int n)
+        {
+            int roll = 0;
+            while (n-- > 0) roll += D10();
+            return roll;
+        }
+
+        /// <summary>
+        /// Returns a random number on a 12-sided dice [1..12].
+        /// </summary>
+        public static int D12()
+        {
+            return UnityEngine.Random.Range(1, 13);
+        }
+
+        /// <summary>
+        /// Returns the sum of rolling a 12-sided dice [1..12]
+        /// <paramref name="n"/> times.
+        /// </summary>
+        /// <param name="n">The number of times to roll the dice.</param>
+        public static int D12(int n)
+        {
+            int roll = 0;
+            while (n-- > 0) roll += D12();
+            return roll;
+        }
+
+        /// <summary>
+        /// Returns a random number on a 20-sided dice [1..20].
+        /// </summary>
+        public static int D20()
+        {
+            return UnityEngine.Random.Range(1, 21);
+        }
+
+        /// <summary>
+        /// Returns the sum of rolling a 20-sided dice [1..20]
+        /// <paramref name="n"/> times.
+        /// </summary>
+        /// <param name="n">The number of times to roll the dice.</param>
+        public static int D20(int n)
+        {
+            int roll = 0;
+            while (n-- > 0) roll += D20();
+            return roll;
+        }
+
+        /// <summary>
+        /// Returns a random number on an n-sided dice [1..<paramref name="n"/>].
+        /// </summary>
+        /// <param name="n">The number of sides on the dice.</param>
+        public static int NSided(int n)
+        {
+            return UnityEngine.Random.Range(1, n + 1);
+        }
+
+        /// <summary>
+        /// Returns the sum of rolling an n-sided dice [1..<paramref name="n"/>]
+        /// <paramref name="x"/> amount of times.
+        /// </summary>
+        /// <param name="n">The number of sides on the dice.</param>
+        /// <param name="x">The number of times to roll the dice.</param>
+        public static int NSided(int n, int x)
+        {
+            int roll = 0;
+            while (x-- > 0) roll += NSided(n);
+            return roll;
+        }
+
+        /// <summary>
+        /// Returns a random value on a dice with custom values.
+        /// </summary>
+        /// <param name="values">The values of the dice.</param>
+        public static int Roll(int[] values)
+        {
+            return values[UnityEngine.Random.Range(0, values.Length)];
+        }
+
+        /// <summary>
+        /// Returns the sum of rolling a dice with custom values
+        /// <paramref name="n"/> amount of times.
+        /// </summary>
+        /// <param name="values">The values of the dice.</param>
+        /// <param name="n">The number of times to roll the dice.</param>
+        public static int Roll(int[] values, int n)
+        {
+            int roll = 0;
+            while (n-- > 0) roll += Roll(values);
+            return roll;
+        }
+
+        /// <summary>
+        /// Returns a random value on a dice with custom values.
+        /// </summary>
+        /// <param name="values">The values of the dice.</param>
+        /// <typeparam name="T">The type of value to choose.</typeparam>
+        public static T Roll<T>(T[] values)
+        {
+            return values[UnityEngine.Random.Range(0, values.Length)];
+        }
+
+        /// <summary>
+        /// Returns a random value from an array of <paramref name="values"/>
+        /// with probability proportional to the given set of
+        /// <paramref name="probabilities"/>.
+        /// </summary>
+        /// <param name="values">The values to choose from.</param>
+        /// <param name="probabilities">The probabilities of each value.</param>
+        /// <typeparam name="T">The type of value to choose.</typeparam>
+        public static T Roll<T>(T[] values, int[] probabilities)
+        {
+            // OPTIMIZE: Prevent allocation of a new array each time.
+            T[] extendedRange = new T[probabilities.Sum()];
+
+            int length = System.Math.Min(values.Length, probabilities.Length);
+            int count = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                T value = values[i];
+                int frequency = probabilities[i];
+
+                while (--frequency > 0) {
+                    extendedRange[count++] = value;
+                }
+            }
+
+            return extendedRange[UnityEngine.Random.Range(0, extendedRange.Length)];
+        }
+
+    }
+
+}
