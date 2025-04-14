8e373cad0be62e9fca852c63c96a20bff06649ab
Adam Graham <adam@zigurous.com>
2025-04-13 23:17:04 -0500

    Add unscaled transform point functions

--- Runtime/Extensions/TransformExtensions.cs
+++ Runtime/Extensions/TransformExtensions.cs
@@ -40,6 +40,30 @@ namespace Zigurous.Math
         }
 
         /// <summary>
+        /// Transforms a point from local space to world space without scaling.
+        /// </summary>
+        /// <param name="transform">The transform to use for the transformation.</param>
+        /// <param name="position">The local position to transform.</param>
+        /// <returns>The transformed world position.</returns>
+        public static Vector3 TransformPointUnscaled(this Transform transform, Vector3 position)
+        {
+            var localToWorldMatrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+            return localToWorldMatrix.MultiplyPoint3x4(position);
+        }
+
+        /// <summary>
+        /// Transforms a point from world space to local space without scaling.
+        /// </summary>
+        /// <param name="transform">The transform to use for the transformation.</param>
+        /// <param name="position">The world position to transform.</param>
+        /// <returns>The transformed local position.</returns>
+        public static Vector3 InverseTransformPointUnscaled(this Transform transform, Vector3 position)
+        {
+            var worldToLocalMatrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one).inverse;
+            return worldToLocalMatrix.MultiplyPoint3x4(position);
+        }
+
+        /// <summary>
         /// Resets the position, rotation, and scale of the transform.
         /// </summary>
         /// <param name="transform">The transform to reset.</param>
